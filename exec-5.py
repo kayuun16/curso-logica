1c76a46cf00141971a54754a8154f875a1cbc540
Kauan Lucas <klasgamer3@gmail.com>
2024-09-01 17:36:48 -0300

    Add files via upload

---
+++ exec-5.py
@@ -0,0 +1,15 @@
+# 5. Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+
+# ALUNO(A)          NOTA
+# =========         =====
+# ALINE              9.0  
+# MÁRIO              DEZ
+# SÉRGIO             4.5    
+# SHIRLEY            7.0
+
+print('ALUNO(A)' + ' ' * 10 + 'NOTA')
+print('=' * 9 + ' ' * 9 + '=' * 5)
+print('ALINE' + ' ' * 14 + '9.0')
+print('MARIO' + ' ' * 14 + 'DEZ')
+print('SERGIO' + ' ' * 13 + '4.5')
+print('SHIRLEY' + ' ' * 12 + '7.0')
