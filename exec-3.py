c06c6df912be90cf3505d07580c80685e748f2a2
Kauan Lucas <klasgamer3@gmail.com>
2024-09-01 17:17:50 -0300

    Add files via upload

---
+++ exec-3.py
@@ -0,0 +1,5 @@
+# 3. Letra de música - Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber).
+
+musica = str(input('Digite o nome de uma musica que voce gosta: '))
+
+print(musica)
