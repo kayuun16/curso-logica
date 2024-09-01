ffd0280798898d2bee73bb26da679983e6b5f035
Kauan Lucas <klasgamer3@gmail.com>
2024-09-01 17:12:54 -0300

    Add files via upload

---
+++ exec-2.py
@@ -0,0 +1,11 @@
+# 2. Etiqueta - Elabore um programa que imprimaseu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+nome = str(input('Qual o seu nome? '))
+
+estado = str(input('Voce mora em qual estado? '))
+
+municipio = str(input('Voce mora em qual municipio? '))
+
+print(f'Nome: {nome}')
+print(f'Estado: {estado}')
+print(f'Municipio: {municipio}')
